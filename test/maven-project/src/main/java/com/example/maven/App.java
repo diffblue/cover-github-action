c8763d0ab279f895bc2e89a3e4631d541cdc6b59
Rob Oxspring <rob.oxspring@diffblue.com>
2023-08-09 16:55:33 +0100

    Added Maven test project

---
+++ test/maven-project/src/main/java/com/example/maven/App.java
@@ -0,0 +1,13 @@
+package com.example.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ test/maven-project/src/main/java/com/example/maven/Math.java
@@ -0,0 +1,7 @@
+package com.example.maven;
+
+public class Math {
+  public static boolean greaterThanFive(int input) {
+    return input > 5;
+  }
+}
