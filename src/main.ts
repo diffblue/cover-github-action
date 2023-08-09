fcfe36f3f3bbcf833b48ce0c40f7d6c7324b5018
Rob Oxspring <rob.oxspring@diffblue.com>
2023-08-09 20:51:31 +0100

    Updated main function to install latest Diffblue Cover version
    Dropped broken default tests for now

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,14 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as exec from '@actions/exec'
+
+import {installLatestVersion} from './install-latest-version'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    core.startGroup('Install Diffblue Cover')
+    await installLatestVersion()
+    await exec.exec('dcover', ['--version'])
+    core.endGroup()
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
