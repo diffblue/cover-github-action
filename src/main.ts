9ec05d80af9ae2764ab1841b8be73ddbfd28747b
Rob Oxspring <rob.oxspring@diffblue.com>
2023-08-10 15:23:35 +0100

    Updated main function to activate the given licence key

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,10 @@ async function run(): Promise<void> {
     core.startGroup('Install Diffblue Cover')
     await installLatestVersion()
     await exec.exec('dcover', ['--version'])
+
+    const key: string = core.getInput('license-key')
+    await exec.exec('dcover', ['activate', key])
+
     core.endGroup()
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
