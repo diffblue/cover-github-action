714e24ece5750dd27e5d3986a3672728fa83e8a6
Rob Oxspring <rob.oxspring@diffblue.com>
2023-08-14 14:59:38 +0100

    Uploaded artifact includes **/.diffblue/**
    Ensuring we capture logs even from runs within a subdirectory

--- src/upload.ts
+++ src/upload.ts
@@ -7,26 +7,24 @@ import {Status} from './status-model'
  * Upload Diffblue Cover logs and other run artifacts.
  * Artifacts are named using the topic slug so that multiple
  * occurrences lead to uniquely named artifacts.
+ * All files within any `.diffblue` directory are included.
  *
  * @param status the status to find the topic slug from.
  */
 export async function upload(status: Status): Promise<void> {
   core.startGroup(`Upload ${status.topic_slug}.zip artifact`)
-  const rootDir = '.diffblue'
-
-  const globber = await glob.create(rootDir)
+  const globber = await glob.create('**/.diffblue/**')
   const paths = await globber.glob()
   for (const path of paths) {
     core.info(`Including ${path}`)
   }
-
   if (paths.length > 0) {
     await artifacts
       .create()
       .uploadArtifact(
         status.topic_slug,
         paths,
-        rootDir,
+        '.',
         {} as artifacts.UploadOptions
       )
   }
