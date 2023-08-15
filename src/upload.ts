6539bad37cf74444bd2ac9ac376a06a4470c8b16
Rob Oxspring <rob.oxspring@diffblue.com>
2023-08-15 18:23:53 +0100

    Uploaded artifact function avoids uploading empty zip

--- src/upload.ts
+++ src/upload.ts
@@ -1,10 +1,6 @@
 import * as core from '@actions/core'
 import * as artifacts from '@actions/artifact'
 import * as glob from '@actions/glob'
-import {stat} from 'fs'
-import {promisify} from 'util'
-
-const stats = promisify(stat)
 
 /**
  * Upload Diffblue Cover logs and other run artifacts.
@@ -12,20 +8,17 @@ const stats = promisify(stat)
 export async function upload(): Promise<void> {
   core.startGroup('Upload diffblue.zip artifact')
   const rootDir = '.diffblue'
-  const files: string[] = []
 
   const globber = await glob.create(rootDir)
-  const rawSearchResults: string[] = await globber.glob()
-  for (const path of rawSearchResults) {
-    const pathStats = await stats(path)
-    if (!pathStats.isDirectory()) {
-      core.info(`Including ${path} in upload`)
-      files.push(path)
-    }
+  const paths = await globber.glob()
+  for (const path of paths) {
+    core.info(`Including ${path}`)
   }
 
-  await artifacts
-    .create()
-    .uploadArtifact('diffblue', files, rootDir, {} as artifacts.UploadOptions)
+  if (paths.length > 0) {
+    await artifacts
+      .create()
+      .uploadArtifact('diffblue', paths, rootDir, {} as artifacts.UploadOptions)
+  }
   core.endGroup()
 }
