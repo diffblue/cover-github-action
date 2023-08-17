96ca13f229368ddf09b74bf3a6a71d32520761f2
Rob Oxspring <rob.oxspring@diffblue.com>
2023-08-17 09:42:09 +0100

    Extracted cover.activate() function to manage activation

--- src/cover.ts
+++ src/cover.ts
@@ -13,6 +13,26 @@ export async function install(status: Status): Promise<void> {
   core.startGroup('Install')
   await installLatestVersion(status)
   await exec.exec('dcover', ['--version'])
+  core.endGroup()
   saveStatus(status)
+}
+
+/**
+ * Runs `dcover activate` using the `license-key` inputs.
+ */
+export async function activate(): Promise<void> {
+  core.startGroup('Activate')
+  const keyName = 'license-key'
+  const keyValue: string = core.getInput(keyName)
+  if (keyValue === '') {
+    throw new Error(
+      [
+        `Missing '${keyName}' configuration:`,
+        `Please ensure that the action has a '${keyName}' configured, typically using a GitHub secret.`,
+        `Please ensure that all users pushing to the repository has access to the necessary secret.`
+      ].join('\n')
+    )
+  }
+  await exec.exec('dcover', ['activate', keyValue])
   core.endGroup()
 }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import * as core from '@actions/core'
-import * as exec from '@actions/exec'
 import * as git from './git'
 import * as cover from './cover'
 import {skip} from './skip'
@@ -16,22 +15,7 @@ async function run(): Promise<void> {
   try {
     await git.prepare(status)
     await cover.install(status)
-
-    core.startGroup('Activate')
-    const keyName = 'license-key'
-    const keyValue: string = core.getInput(keyName)
-    if (keyValue === '') {
-      throw new Error(
-        [
-          `Missing '${keyName}' configuration:`,
-          `Please ensure that the action has a '${keyName}' configured, typically using a GitHub secret.`,
-          `Please ensure that all users pushing to the repository has access to the necessary secret.`
-        ].join('\n')
-      )
-    }
-    await exec.exec('dcover', ['activate', keyValue])
-
-    core.endGroup()
+    await cover.activate()
   } catch (error) {
     status.error = error
     if (error instanceof Error) {
