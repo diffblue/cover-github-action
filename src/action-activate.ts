1525d4b228cf1ea97cecb54a51a20884a0086fc9
Rob Oxspring <rob.oxspring@diffblue.com>
2023-08-21 23:22:12 +0100

    Extracted component action for: dcover activate

---
+++ src/action-activate.ts
@@ -0,0 +1,33 @@
+import * as core from '@actions/core'
+import * as cover from './internal/cover'
+import {skip} from './internal/skip'
+import {upload} from './internal/upload'
+import {readStatus, saveStatus} from './internal/status-io'
+
+/**
+ * Runs the "activate" isolated action.
+ */
+async function run(): Promise<void> {
+  if (await skip()) {
+    return
+  }
+
+  const status = await readStatus()
+  try {
+    await cover.activate()
+  } catch (error) {
+    status.error = error
+    if (error instanceof Error) {
+      core.setFailed(error.message)
+      if (error.stack) {
+        core.info(error.stack)
+      }
+    }
+  }
+
+  await saveStatus(status)
+  await cover.cleanup()
+  await upload(status)
+}
+
+run()
