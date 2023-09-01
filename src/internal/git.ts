1aaac33c5aa2e5b036776fd4dc38768138b07dda
Rob Oxspring <rob.oxspring@diffblue.com>
2023-09-01 18:10:02 +0100

    Added commit-and-push configuration

--- src/internal/git.ts
+++ src/internal/git.ts
@@ -1,12 +1,17 @@
+import * as core from '@actions/core'
 import * as exec from '@actions/exec'
 import {Status} from './status-model'
 
 /**
  * Runs `git commit` to commit modified files.
+ * Unless `commit-and-push` is configured to `false`.
  *
  * @param status the status to be updated and saved.
  */
 export async function commit(status: Status, message: string): Promise<void> {
+  if (!enabled()) {
+    return
+  }
   const result = await exec.getExecOutput('git', [
     'status',
     '--untracked-files=all',
@@ -27,9 +32,23 @@ export async function commit(status: Status, message: string): Promise<void> {
 
 /**
  * Runs `git push` to push changes up.
+ * Unless `commit-and-push` is configured to `false`.
  *
  * @param status the status to be updated and saved.
  */
 export async function push(status: Status): Promise<void> {
+  if (!enabled()) {
+    return
+  }
   await exec.exec('git', ['push', 'origin', `HEAD:${status.ref}`])
+}
+
+/**
+ * @returns true iff the `commit-and-push` configuration allows committing and pushing.
+ */
+function enabled(): boolean {
+  return (
+    core.getInput('commit-and-push') === '' ||
+    core.getInput('commit-and-push') === 'true'
+  )
 }
