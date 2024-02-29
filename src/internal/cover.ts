b147d71ce2e17e5ece8a75220c6a127e80985713
Rob Oxspring <rob.oxspring@diffblue.com>
2024-02-29 17:27:18 +0000

    Explicit dcover create is out of scope

--- src/internal/cover.ts
+++ src/internal/cover.ts
@@ -1,18 +1,9 @@
-import * as glob from '@actions/glob'
 import * as core from '@actions/core'
 import * as exec from '@actions/exec'
-import * as fs from 'fs'
-import * as path from 'path'
 import {installLatestVersion} from './install-latest-version'
-import {Report, Status} from './status-model'
+import {Status} from './status-model'
 import {saveStatus} from './status-io'
-import {readFileSync} from 'fs'
 
-declare let process: {
-  env: {
-    GITHUB_WORKSPACE: string
-  }
-}
 
 /**
  * Installs `dcover` for use by other commands and functions.
@@ -64,26 +55,6 @@ export async function clean(status: Status): Promise<void> {
 }
 
 /**
- * Runs `dcover create --pre-flight`.
- * Additionally uses `--fix-build` and commits any resulting fixes.
- *
- * @param status the status to be updated and saved.
- */
-export async function createPreFlight(status: Status): Promise<void> {
-  core.startGroup('Create (pre-flight)')
-  await exec.exec('dcover', [
-    'create',
-    '--batch',
-    '--pre-flight',
-    '--fix-build',
-    ...workingDirectoryArgs(),
-    ...coverReportsArgs(status),
-    ...extraArgs('create-args')
-  ])
-  core.endGroup()
-}
-
-/**
  * @returns the `--working-directory` arguments based on `working-directory` input, or an empty array.
  */
 function workingDirectoryArgs(): string[] {
@@ -93,31 +64,6 @@ function workingDirectoryArgs(): string[] {
   } else {
     return []
   }
-}
-
-/**
- * @param status the status to derive project argument.
- * @returns the `--coverage-reports` arguments based on `cover-reports-url` input, or an empty array.
- */
-function coverReportsArgs(status: Status): string[] {
-  const url = core.getInput(`cover-reports-url`)
-  if (url === '') {
-    return []
-  }
-
-  // Smoke & Mirrors:
-  // Obviously this should make use of the url above, but
-  // the demo uses an SSH tunnel and the correct URL is never
-  // derived, so we end up hacking it together here instead:
-  status.cover_reports_url = `https://demo.reports.diffblue.co.uk/ui/project/${status.owner}/${status.repo}`
-
-  return [
-    `--coverage-reports`,
-    `--report`,
-    url,
-    `--project`,
-    `${status.owner}.${status.repo}`
-  ]
 }
 
 /**
