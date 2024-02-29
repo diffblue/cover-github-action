48ca7ce202a765844ae49114a92f1b7ec60f2d26
Rob Oxspring <rob.oxspring@diffblue.com>
2024-02-29 18:11:42 +0000

    Drop extra disclosure group

--- src/internal/cover.ts
+++ src/internal/cover.ts
@@ -1,4 +1,3 @@
-import * as core from '@actions/core'
 import * as exec from '@actions/exec'
 import {installLatestVersion} from './install-latest-version'
 
@@ -6,8 +5,6 @@ import {installLatestVersion} from './install-latest-version'
  * Installs `dcover` for use by other commands and functions.
  */
 export async function install(): Promise<void> {
-  core.startGroup('Install')
   await installLatestVersion()
   await exec.exec('dcover', ['--version'])
-  core.endGroup()
 }
