c522f69eb01d08526178bf0574011ff533f25498
Rob Oxspring <rob.oxspring@diffblue.com>
2024-03-12 22:11:43 +0000

    Trust dcover to process DIFFBLUE_WORKING_DIRECTORY

--- jdk21/entrypoint.sh
+++ jdk21/entrypoint.sh
@@ -11,9 +11,5 @@ git fetch --all --deepen=5
 # Reset the image-specific environment
 export $(cat /.env | xargs)
 
-# Just change directory here because it's easier than
-# correctly adding --working-directory to each command
-cd "${DIFFBLUE_WORKING_DIRECTORY}"
-
 # The command to run. Should ci and activate be mandatory?
 /opt/cover/dcover ${INPUT_ARGS}
